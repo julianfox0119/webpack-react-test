91eacea627d931e76967802951f67cdca2734eb0
julianfox0119 <julianfox0119@gmail.com>
2017-06-05 12:45:53 +0800

    add submenu

--- app/components/common/Nav.js
+++ app/components/common/Nav.js
@@ -4,6 +4,9 @@ import { Menu, Icon } from 'antd';
 
 import { Link } from 'react-router-dom'
 
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 class Nav extends Component {
 
   constructor(props) {
@@ -34,6 +37,16 @@ class Nav extends Component {
         <Menu.Item key="single">
           <Link to="/single"><Icon type="appstore" />Single</Link>
         </Menu.Item>
+        <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+          <MenuItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
       </Menu>
     );
   }
